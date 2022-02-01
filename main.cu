f93a11a5fb989f39d336004294e60544e54c6fef
Ryuichi Sai <ryuichi@rice.edu>
2022-01-31 18:52:12 -0600

    Additional hello world

--- main.cu
+++ main.cu
@@ -1,4 +1,29 @@
-__global__ void f4_1_kernel(const float *__restrict__ u) {
+#include <stdio.h>
+
+__global__ void f_kernel(const float *__restrict__ u, float *__restrict__ v) {
+  int i = threadIdx.x * 4;
+  v[i+0] = u[i+0];
+  v[i+1] = u[i+1];
+  v[i+2] = u[i+2];
+  v[i+3] = u[i+3];
+}
+
+__global__ void f4_kernel(const float *__restrict__ u, float *__restrict__ v) {
+  int i = threadIdx.x * 4;
+  float4 *u4_ = (float4 *) (&u[i]);
+  float4 u4 = u4_[0];
+  float4 v4;
+  v4.x = u4.x;
+  v4.y = u4.y;
+  v4.z = u4.z;
+  v4.w = u4.w;
+  v[i+0] = v4.x;
+  v[i+1] = v4.y;
+  v[i+2] = v4.z;
+  v[i+3] = v4.w;
+}
+
+__global__ void f4_p1_kernel(const float *__restrict__ u, float *__restrict__ v) {
   int i = threadIdx.x * 4;
   float4 *u4_ = (float4 *) (&u[i]);
   float4 u4 = u4_[0];
@@ -7,9 +32,32 @@ __global__ void f4_1_kernel(const float *__restrict__ u) {
   v4.y = u4.y + 1;
   v4.z = u4.z + 1;
   v4.w = u4.w + 1;
+  v[i+0] = v4.x;
+  v[i+1] = v4.y;
+  v[i+2] = v4.z;
+  v[i+3] = v4.w;
 }
 
-static void f4_1() {
+__global__ void f4_pv_kernel(const float *__restrict__ u, float *__restrict__ v) {
+  int i = threadIdx.x * 4;
+
+  float4 *u4_ = (float4 *) (&u[i]);
+  float4 u4 = u4_[0];
+  float4 *v4_ = (float4 *) (&v[i]);
+  float4 v4 = v4_[0];
+
+  v4.x = u4.x + v4.x;
+  v4.y = u4.y + v4.y;
+  v4.z = u4.z + v4.z;
+  v4.w = u4.w + v4.w;
+
+  v[i+0] = v4.x;
+  v[i+1] = v4.y;
+  v[i+2] = v4.z;
+  v[i+3] = v4.w;
+}
+
+static void f4_p1() {
   int size = 16;
   int mem_size = sizeof(float) * 16;
 
@@ -22,14 +70,25 @@ static void f4_1() {
   cudaMalloc((void **)&d_u, mem_size);
   cudaMemcpy(d_u, u, mem_size, cudaMemcpyHostToDevice);
 
-  f4_1_kernel<<<1, size / 4>>>(d_u);
+  float *d_v;
+  cudaMalloc((void **)&d_v, mem_size);
+  cudaMemcpy(d_v, u, mem_size, cudaMemcpyHostToDevice);
 
+  f4_p1_kernel<<<1, size / 4>>>(d_u, d_v);
+
+  cudaMemcpy(u, d_v, mem_size, cudaMemcpyDeviceToHost);
+
+  for (int i = 0; i < size; i++) {
+    printf("%d:\t %f\n", i, u[i]);
+  }
+
+  cudaFree(d_v);
   cudaFree(d_u);
   free(u);
 }
 
 int main() {
-    f4_1();
+    f4_p1();
     // f4_2();
 
     return 0;
--- run.sh
+++ run.sh
@@ -1,1 +1,2 @@
-nvcc -O3 --ptxas-options=-v --use_fast_math main.cu
+nvcc -arch=sm_80 -O3 --ptxas-options=-v --use_fast_math main.cu
+cuobjdump a.out -sass -ptx > a.dump
